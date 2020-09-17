d7408227de5732bfd66cefbe69ac58646320050a
gabriellahartanto <gabriella.hartanto@icloud.com>
2020-09-17 20:27:38 +0700

    - initialize client - vue create - vue add vuex - add components: Board, Card, Leaderboard - add views: Home, Landing - fix routers - add emoji png

---
+++ client/src/router/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Landing from '../views/Landing.vue'
+import NotFound from '../views/NotFound.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/play',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/',
+    name: 'Landing',
+    component: Landing
+  },
+  {
+    path: '/404',
+    name: 'NotFound',
+    component: NotFound
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (!localStorage.username && to.name !== 'Login') next({ name: 'Login' })
+  else next()
+})
+
+export default router
